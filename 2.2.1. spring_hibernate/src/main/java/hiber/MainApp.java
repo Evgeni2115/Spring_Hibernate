978f98efc86642da3fdffb297208b9a82fafa1da
Евгений Решетников <122768108+Evgeni2115@users.noreply.github.com>
2023-04-10 19:44:08 +0500

    first commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,28 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Userovich", "user@mail.ru");
+      User user2 = new User("User2", "Userovich", "user@mail.ru");
+      User user3 = new User("User3", "Userovich", "user@mail.ru");
+      User user4 = new User("User4", "Userovich", "user@mail.ru");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car audi = new Car(7, "Audi");
+      Car bmw = new Car(5, "BMW");
+      Car kia = new Car(1, "Kia");
+      Car landRover = new Car(1, "LandRover");
+
+      userService.add(user1.setCar(audi).setUser(user1));
+      userService.add(user2.setCar(bmw).setUser(user2));
+      userService.add(user3.setCar(kia).setUser(user3));
+      userService.add(user4.setCar(landRover).setUser(user4));
+
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
       }
+
+
+      System.out.println(userService.getUserCar("BMW", 5));
 
       context.close();
    }
